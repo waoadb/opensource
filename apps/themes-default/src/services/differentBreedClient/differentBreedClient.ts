c52561151acf2f07387564128d26c5bcfae0b9ca
Craig Pryde <craig@differentbreed.digital>
2023-04-17 02:25:52 +0100

    env naming updated

--- apps/themes-default/src/services/differentBreedClient/differentBreedClient.ts
+++ apps/themes-default/src/services/differentBreedClient/differentBreedClient.ts
@@ -4,6 +4,5 @@ import { DifferentBreedClient } from '@waoadb/js-client-sdk';
 // Export client instance.
 export const differentBreedClient = new DifferentBreedClient({
   profileId: process.env.NEXT_PUBLIC_DIFFERENT_BREED_PROFILE_ID as string,
-  clientAPIKey: process.env
-    .NEXT_PUBLIC_DIFFERENT_BREED_PROFILE_CLIENT_API_KEY as string,
+  clientAPIKey: process.env.NEXT_PUBLIC_DIFFERENT_BREED_CLIENT_KEY as string,
 });
